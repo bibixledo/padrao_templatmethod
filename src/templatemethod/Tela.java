97990a2ab78d361316b7a1054fda265e2cf03691
puc <puc@lab-ecec>
2018-09-28 10:47:12 -0300

    teste

--- src/templatemethod/Tela.java
+++ src/templatemethod/Tela.java
@@ -6,11 +6,13 @@ import javax.swing.table.DefaultTableModel;
 
 
 public class Tela extends javax.swing.JFrame {
+    
+    List<Professor> dadosProf;
 
     public Tela() {
         initComponents();
         PersistenciaLista dados = new PersistenciaLista();
-        
+        dadosProf = dados.getLista();
         DefaultTableModel model = (DefaultTableModel) grid_Dados.getModel();
         Iterator<Professor> it = dados.getDados();
         //List<Professor> teste = new SortDepart().bubbleSort(dados.getLista());
@@ -31,6 +33,12 @@ public class Tela extends javax.swing.JFrame {
         labelTitle = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         grid_Dados = new javax.swing.JTable();
+        jCheckBox1 = new javax.swing.JCheckBox();
+        jCheckBox2 = new javax.swing.JCheckBox();
+        jCheckBox3 = new javax.swing.JCheckBox();
+        jCheckBox4 = new javax.swing.JCheckBox();
+        jCheckBox5 = new javax.swing.JCheckBox();
+        jToggleButton1 = new javax.swing.JToggleButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         menuMain = new javax.swing.JMenu();
 
@@ -63,6 +71,23 @@ public class Tela extends javax.swing.JFrame {
         });
         jScrollPane1.setViewportView(grid_Dados);
 
+        jCheckBox1.setText("Ord. Nome");
+
+        jCheckBox2.setText("Ord. Matricula");
+
+        jCheckBox3.setText("Ord. Titulacao");
+
+        jCheckBox4.setText("Ord. Departamento");
+
+        jCheckBox5.setText("Ord. Horista");
+
+        jToggleButton1.setText("Atualizar");
+        jToggleButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButton1ActionPerformed(evt);
+            }
+        });
+
         menuMain.setText("Ordenar");
         jMenuBar1.add(menuMain);
 
@@ -76,9 +101,25 @@ public class Tela extends javax.swing.JFrame {
                 .addGap(236, 236, 236)
                 .addComponent(labelTitle)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 565, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 547, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jCheckBox1)
+                                .addGap(18, 18, 18)
+                                .addComponent(jCheckBox3)
+                                .addGap(30, 30, 30)
+                                .addComponent(jCheckBox5))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jCheckBox2)
+                                .addGap(18, 18, 18)
+                                .addComponent(jCheckBox4)
+                                .addGap(26, 26, 26)
+                                .addComponent(jToggleButton1)))
+                        .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -87,11 +128,46 @@ public class Tela extends javax.swing.JFrame {
                 .addComponent(labelTitle)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 252, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jCheckBox1)
+                    .addComponent(jCheckBox3)
+                    .addComponent(jCheckBox5))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jCheckBox2)
+                        .addComponent(jCheckBox4))
+                    .addComponent(jToggleButton1))
+                .addGap(0, 17, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jToggleButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton1ActionPerformed
+        if(this.jCheckBox1.isSelected())
+            dadosProf = new SortNome().bubbleSort(dadosProf);
+        if(this.jCheckBox2.isSelected())
+            dadosProf = new SortMatricula().bubbleSort(dadosProf);
+        if(this.jCheckBox3.isSelected())
+            dadosProf = new SortTitulacao().bubbleSort(dadosProf);
+        if(this.jCheckBox4.isSelected())
+            dadosProf = new SortDepart().bubbleSort(dadosProf);
+        if(this.jCheckBox5.isSelected())
+            dadosProf = new SortHorst().bubbleSort(dadosProf);
+        DefaultTableModel model = (DefaultTableModel) grid_Dados.getModel();
+        Iterator<Professor> it = dadosProf.iterator();
+        for(int i = 0; i < model.getRowCount();i++){
+            model.removeRow(i);
+        }
+        int i = 0;
+          while(it.hasNext()){
+            Professor aux = it.next();
+            String[] x = {aux.getMatricula(),aux.getNome(),aux.getDepartamento(),aux.getTitulacao(),aux.getHorista()};
+            model.addRow(x);
+        }
+    }//GEN-LAST:event_jToggleButton1ActionPerformed
 
     public static void main(String args[]) {
         
@@ -104,8 +180,14 @@ public class Tela extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTable grid_Dados;
+    private javax.swing.JCheckBox jCheckBox1;
+    private javax.swing.JCheckBox jCheckBox2;
+    private javax.swing.JCheckBox jCheckBox3;
+    private javax.swing.JCheckBox jCheckBox4;
+    private javax.swing.JCheckBox jCheckBox5;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JToggleButton jToggleButton1;
     private javax.swing.JLabel labelTitle;
     private javax.swing.JMenu menuMain;
     // End of variables declaration//GEN-END:variables
